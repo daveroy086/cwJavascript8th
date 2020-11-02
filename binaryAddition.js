309b93f6875b0a0149e012e0c38c6cb400c016a0
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-11-02 14:27:51 -0500

    send to github

---
+++ binaryAddition.js
@@ -0,0 +1,8 @@
+//...cw called it addBinary(a, b)
+function binaryAddition(a, b){
+  var sum = a + b;
+  //return sum.toString(2);
+  console.log(sum.toString(2));
+}
+//test
+binaryAddition(200, 55);
